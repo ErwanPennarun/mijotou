73dc5855a407fb53566d1092f224352646a534e7
ErwanPennarun <erwan.pennarun@gmail.com>
2022-11-08 16:13:40 +0100

    switching with initialprops

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Card from "../components/Card";
 import { motion } from "framer-motion";
 import { useState } from "react";
 
-export const getStaticProps = async () => {
+export const getInitialProps = async () => {
   const res = await fetch(
     `http://mijoutouapi-env.eba-btjfdght.eu-west-3.elasticbeanstalk.com/recettes/`
   );
