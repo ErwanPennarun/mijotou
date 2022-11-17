997bb309193623c7ef1189195c76c49e1893b5f2
ErwanPennarun <erwan.pennarun@gmail.com>
2022-11-17 13:33:19 +0100

    small fixes

--- pages/recettes/[id].js
+++ pages/recettes/[id].js
@@ -3,7 +3,6 @@ import Image from "next/image";
 import { PlusCircleIcon, MinusCircleIcon } from "@heroicons/react/24/solid";
 import { motion } from "framer-motion";
 import Link from "next/link";
-import Recettes from "../recettes";
 
 export const getStaticPaths = async () => {
   const res = await fetch("https://mijotou-api.herokuapp.com/recettes/");
@@ -66,47 +65,48 @@ const Recipe = ({ recipe }) => {
           <Link href={recette.source_url}>par {recette.author}</Link>
         )}
 
-        <div className="grid grid-cols-3 gap-10">
-          {recette.servings && (
-            <div className="flex flex-row lg:ml-4 justify-center lg:justify-start col-span-3">
-              Pour
-              <MinusCircleIcon
-                className="w-5 h-5"
-                onClick={() => {
-                  setServing(serving - 1);
-                  updateQuantity(serving + 1);
-                }}
-              />
-              {serving}
-              <PlusCircleIcon
-                className="w-5 h-5"
-                onClick={() => {
-                  setServing(serving + 1);
-                  updateQuantity(serving + 1);
-                }}
-              />
-              personnes.
+        <div className="grid grid-cols-3 gap-10 ">
+          <div className="lg:mx-auto col-span-3 lg:col-span-1">
+            {recette.servings && (
+              <div className="flex flex-row lg:ml-4 lg:mb-6 justify-center lg:justify-start ">
+                Pour
+                <MinusCircleIcon
+                  className="w-5 h-5"
+                  onClick={() => {
+                    setServing(serving - 1);
+                    updateQuantity(serving + 1);
+                  }}
+                />
+                {serving}
+                <PlusCircleIcon
+                  className="w-5 h-5"
+                  onClick={() => {
+                    setServing(serving + 1);
+                    updateQuantity(serving + 1);
+                  }}
+                />
+                personnes.
+              </div>
+            )}
+            <div className="col-span-3 lg:col-span-1  lg:border-r max-[1024px]:border-b border-gray-800 border-dotted pr-4 lg:border-opacity-25">
+              <h2 className="text-lg ml-4 uppercase">Ingredients </h2>
+              {recette.ingredients.map((ingredient, index) => {
+                return (
+                  <ul className="mt-2 ml-9 lg:mt-6" key={index}>
+                    <li key={index}>
+                      {quantity[index]}
+                      {ingredient.unit} {ingredient.ingredient}
+                    </li>
+                  </ul>
+                );
+              })}
             </div>
-          )}
-          <div className="col-span-3 lg:col-span-1 lg:border-r max-[1024px]:border-b border-gray-800 border-dotted lg:border-opacity-25">
-            <h2 className="text-lg ml-4 uppercase">Ingredients </h2>
-            {recette.ingredients.map((ingredient, index) => {
-              return (
-                <ul className="mt-2 ml-9 lg:mt-6" key={index}>
-                  <li key={index}>
-                    {quantity[index]}
-                    {ingredient.unit} {ingredient.ingredient}
-                  </li>
-                </ul>
-              );
-            })}
           </div>
-
           <div className="col-span-3 lg:col-span-2">
             <h2 className="text-lg ml-4 uppercase">Quoi que faire ?</h2>
             {recipe.steps.map((step, index) => {
               return (
-                <ul className="mt-2 ml-9 lg:mt-6" key={index}>
+                <ul className="mt-8  mx-6 lg:mt-6" key={index}>
                   <li key={index}>
                      {index + 1}. {step}
                   </li>
